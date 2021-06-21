884257a183c457ed8b7ba0b5ddfe2b8779b95ae4
pobortsevv <pobortcevv@gmail.com>
2021-06-21 16:00:05 +0300

    01 and 02 done

---
+++ day06/ex01/main.cpp
@@ -0,0 +1,80 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *        Version:  1.0
+ *        Created:  21.06.2021 11:12:29
+ *       Compiler:  clang++
+ *
+ *         Author:  sabra
+ *
+ * ===================================================================
+ */
+
+#include <iostream>
+#include <string>
+
+struct Data
+{
+	std::string *str1;
+	int number;
+	std::string *str2;
+};
+
+void * serialize(void)
+{
+	srand(time(nullptr));
+	std::string rand_str[5] = { "school21", "serial", "stroustrup", "pony", "penguin" };
+	std::string *str1 = new std::string(rand_str[rand() % 5]);
+	int number = rand() % 20;
+	std::string *str2 = new std::string(rand_str[rand() % 5]);
+
+	void *raw = ::operator new (sizeof(std::string *) + sizeof(int) + sizeof(std::string *));
+	void *raw_tmp = raw;
+
+	std::cout << std::endl << std::endl;
+
+	*reinterpret_cast<std::string **>(raw_tmp) = str1;
+	raw_tmp = reinterpret_cast<void **>(raw_tmp) + 1;
+	*reinterpret_cast<int *>(raw_tmp) = number;
+	raw_tmp = reinterpret_cast<void **>(raw_tmp) + 1;
+	*reinterpret_cast<std::string **>(raw_tmp) = str2;
+	std::cout << "In serialization:" << std::endl;
+	std::cout << "1st string: " << str1;
+	std::cout << ", " << *str1 << std::endl;
+	std::cout << "number: " << &number;
+	std::cout << ", " << number << std::endl;
+	std::cout << "2nd string: " << str2;
+	std::cout << ", " << *str2 << std::endl;
+
+	return (raw);
+}
+
+Data * deserialize(void * raw)
+{
+	Data *des = new Data;
+
+	std::cout << sizeof(*raw);
+	des->str1 = *reinterpret_cast<std::string **>(raw);
+	raw = reinterpret_cast<void **>(raw) + 1;
+	des->number = *reinterpret_cast<int *>(raw);
+	raw = reinterpret_cast<void **>(raw) + 1;
+	des->str2 = *reinterpret_cast<std::string **>(raw);
+	
+	return des;
+}
+
+int main(void)
+{
+	Data *result = deserialize(serialize());
+	std::cout << std::endl;
+	std::cout << "After deserialization:" << std::endl;
+	std::cout << "1st string: " << result->str1;
+	std::cout << ", " << *result->str1 << std::endl;
+	std::cout << "number: " << &result->number;
+	std::cout << ", " << result->number << std::endl;
+	std::cout << "2nd string: " << result->str2;
+	std::cout << ", " << *result->str2 << std::endl;
+	return 0;
+}
---
+++ day06/ex02/main.cpp
@@ -0,0 +1,85 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *        Version:  1.0
+ *        Created:  21.06.2021 15:27:33
+ *       Compiler:  clang++
+ *
+ *         Author:  sabra
+ *
+ * ===================================================================
+ */
+
+
+#include <iostream>
+
+class Base
+{
+	public:
+		virtual ~Base(void) {};
+};
+
+class A : public Base
+{};
+
+class B : public Base
+{};
+
+class C : public Base
+{};
+
+Base *generate(void)
+{
+	srand(time(nullptr));
+	char arr[3] = { 'A', 'B', 'C'};
+	int n = rand() % 3;
+
+	std::cout << "Generated " << arr[n] << " class" << std::endl;
+		
+	if (n == 0)
+		return new A;
+	else if (n == 1)
+		return new B;
+	else
+		return new C;
+}
+
+void identify_from_pointer(Base * p)
+{
+	A *a_ptr;
+	B *b_ptr;
+	C *c_ptr;
+	
+	std::cout << "\nIdentification from pointer:" << std::endl;
+
+	if ((a_ptr = dynamic_cast<A *>(p)))
+		std::cout << "This is A class" << std::endl;
+	else if ((b_ptr = dynamic_cast<B *>(p)))
+		std::cout << "This is B class" << std::endl;
+	else if ((c_ptr = dynamic_cast<C *>(p)))
+		std::cout << "This is C class" << std::endl;
+}
+
+void identify_from_reference( Base & p)
+{
+	
+	std::cout << "\nIdentification from pointer:" << std::endl;
+
+	if (dynamic_cast<A *>(&p))
+		std::cout << "This is A class" << std::endl;
+	else if (dynamic_cast<B *>(&p))
+		std::cout << "This is B class" << std::endl;
+	else if (dynamic_cast<C *>(&p))
+		std::cout << "This is C class" << std::endl;
+	
+}
+
+int main(void)
+{
+	Base *tmp = generate();
+	identify_from_pointer(tmp);
+	identify_from_reference(*tmp);
+	return 0;
+}
